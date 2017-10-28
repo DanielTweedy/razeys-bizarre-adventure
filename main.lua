e5fdf4dde837a7d13ba5768b69eb9e85d26a0702
MelBrooksKA <MelBrooksKA@users.noreply.github.com>
2017-10-27 21:52:03 -0700

    Added skeleton files for main, player, maps, tiles, and units

---
+++ main.lua
@@ -0,0 +1,20 @@
+--[[
+Razey's Bizzare Adventure
+
+Nick Teves, Hanwei Li, Daniel Tweedy
+
+Turn based top down strategy game where player controls units and fights enemy units. Game follows alchemist Razey, a young man made old by drinking bad potions. Razey travels the world with a motely group of friends and sell swords to find ingrediants for the ultimate potion.
+]]--
+
+
+function love.load()
+    love.window.setTitle("Razey's Bizarre Adventure")
+end
+
+function  love.draw()
+
+end
+
+function love.update(dt)
+
+end
